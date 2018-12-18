0cf3664cb81ee393205233673c69bbddb6779f32
tc-sabr <42155277+tc-sabr@users.noreply.github.com>
2018-12-18 11:35:21 -0700

    import data and partial cast to numbers

--- static/js/app.js
+++ static/js/app.js
@@ -17,4 +17,30 @@ var svg = d3.select("#scatter")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
+//append svg group to hold chart
+var chartGroup = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
+//import data
+d3.csv('data/data.csv').then(function(stateData) {
+    console.log(stateData)
+
+    //cast data as numbers
+    stateData.forEach(function(data) {
+        data.age = +data.age;
+        data.ageMoe = +data.ageMoe;
+        data.healthcare = +data.healthcare;
+        data.healthcareHigh = +data.healthcareHigh;
+        data.healthcareLow = +data.healthcareLow;
+        data.income = +data.income;
+        data.incomeMoe = +data.incomeMoe;
+        data.obesity = +data.obesity;
+        data.obesityHigh = +data.obesityHigh;
+        data.obesityLow = +data.obesityLow;
+        data.poverty = +data.poverty;
+        data.povertyMoe = +data.povertyMoe;
+        data.smokes = +data.smokes;
+        data.smokesHigh = +data.smokesHigh;
+        data.smokesLow = +data.smokesLow;
+    })
+});
