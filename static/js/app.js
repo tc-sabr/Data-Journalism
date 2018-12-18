2baddee61751f198b9dd2a1d87f877f587d7284b
tc-sabr <42155277+tc-sabr@users.noreply.github.com>
2018-12-18 11:14:33 -0700

    add svg space

---
+++ static/js/app.js
@@ -0,0 +1,20 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+//create svg wrapper
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+
